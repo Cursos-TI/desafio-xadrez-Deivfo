b342cf907a45b741a9901d1e28df723717d5f456
Deivfo <deivfo89@gmail.com>
2025-03-05 20:59:46 -0300

    Create xadrez (Mestre).c

---
+++ xadrez (Mestre).c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void MoverTorre(int numero){
+    if (numero > 0){
+        printf("Direita \n");
+        MoverTorre(numero - 1);
+    }    
+}
+
+void MoverBispo(int numero){
+    if (numero > 0){        
+        printf("Cima, Direita \n");
+        MoverBispo(numero - 1);        
+    }    
+}
+
+void MoverRainha(int numero){
+    if (numero > 0){        
+        printf("Esquerda \n");
+        MoverRainha(numero - 1);        
+    }    
+}
+
+int main() {
+    
+    //Movimento da torre
+    int torre = 5;
+    printf("\n");
+    printf("-----Movimento da torre-----\n");
+    printf("\n");
+    MoverTorre(torre);
+
+    //Movimento do bispo
+    int bispo = 5;
+    printf("\n");
+    printf("-----Movimento do bispo-----\n");
+    printf("\n");
+    MoverBispo(bispo);
+
+    //Movimento da rainha
+    int rainha = 7;
+    printf("\n");
+    printf("-----Movimento da rainha-----\n");
+    printf("\n");
+    MoverRainha(rainha);
+
+
+    // Movimento do Cavalo (2 casas para cima e 1 para a direita) com loops aninhados
+    printf("\n");
+    printf("-----Movimento do Cavalo-----\n");
+    printf("\n");
+    for (int i = 0; i < 2; i++) {
+        printf("Cima\n");
+    }
+    for (int j = 0; j < 1; j++) {
+        printf("Direita\n");
+    }
+    printf("\n");
+
+
+return 0;
+}
